e93445948f66f2b0ffea31c5a1593a54578028b9
Douglas Hipolito <me@douglashipolito.com>
2019-11-29 15:27:29 -0300

    Fixing HTTP header issue on proxy

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
+// Replacing the the default http parse by a more flexible/tolerant HTTP parser
+process.binding('http_parser').HTTPParser = require('http-parser-js').HTTPParser;
+
 require('./src/occ-tools');

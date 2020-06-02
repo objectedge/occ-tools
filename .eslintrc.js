57d1c7ac25cbf5f8befc77f29304ec1a6a9715c5
Marc Laurent <dev.marc.laurent@gmail.com>
2020-06-02 00:28:34 -0300

    Adjusting some files from v2.x

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,8 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module"
-    },
-    "rules": {
-        "indent": [2, 2, { "SwitchCase": 1 }],
-        "linebreak-style": [2, "unix"],
-        "quotes": [2, "single"],
-        "semi": [2, "always"]
-    }
+  env: { es2020: true, node: true, commonjs: true },
+  extends: ["plugin:prettier/recommended"],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
 };
